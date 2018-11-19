703b62bbe1a81b6e234f48509f2fb9f1819ae844
Sanjeev Rangaiah <Sanjeev.Rangaiah@tranzact.net>
2018-11-19 08:14:49 -0800

    bug fixes

--- MedicareAmaze/app.ts
+++ MedicareAmaze/app.ts
@@ -74,15 +74,16 @@ class App {
 
                 const bot = new builder.UniversalBot(connector);
                 //bot.set("storage", new MongoDbBotStorage(new MongoDBStorageClient({ db })));
-               /* bot.set("storage", new MongoDbBotStorage(new MongoDBStorageClient({
+                bot.set("storage", new MongoDbBotStorage(new MongoDBStorageClient({
                     url: process.env.MONGODB_PROVIDER_DEV,
                     mongoOptions: {}
                 })));
-                bot.set('persistConversationData', true);    */
+                bot.set('persistConversationData', true);    
                 const intents = new builder.IntentDialog();
                 bot.dialog('/', intents);
 
-                intents.matches(/^(help|hi|hello)/i, [
+                //intents.matches(/^(help|hi|hello)/i, [
+                intents.matches(/.*/i, [
                     session => {
                         session.send('Hi, Welcome to Initial Opinion - your free initial diagnosis tool');
                         bot.beginDialog(session.message.address, '/diagnosis');
